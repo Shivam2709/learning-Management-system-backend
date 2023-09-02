c799b96138c97acb61f46edf96b5765859c0a7c1
Shivam2709 <shivamthakur2709@gmail.com>
2023-09-02 17:01:57 +0530

    fotter completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 
 import './App.css'
 
+//import { Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
+
 function App() {
   
 
   return (
     <>
-      LMS
+      {/* <Routes> */}
+        {/*<Route path="/" element={<Home />}></Route> */}
+      {/* </Routes> */}
+
+      <Footer />
     </>
   )
 }
