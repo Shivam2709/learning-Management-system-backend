05d021f8994bce4219876b90b69838dac73f97e7
Shivam2709 <shivamthakur2709@gmail.com>
2023-10-07 20:33:38 +0530

    About page complited

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,26 @@
 
 import './App.css'
 
-//import { Routes } from 'react-router-dom';
-import Footer from './Components/Footer';
+import { Route, Routes } from 'react-router-dom';
+
+import AboutUs from './Pages/AboutUs';
+// import HomeLayout from './Layouts/HomeLayout';
+import HomePage from './Pages/HomePage';
+
 
 function App() {
   
 
   return (
     <>
-      {/* <Routes> */}
-        {/*<Route path="/" element={<Home />}></Route> */}
-      {/* </Routes> */}
+      <Routes> 
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/about" element={<AboutUs />}></Route>
+      </Routes>
 
-      <Footer />
+   
+  
+      
     </>
   )
 }
