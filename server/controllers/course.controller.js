a1a3a79d69db4f28d6742f90ff94f91aeae03ccf
Shivam Thakur <shivamthakur2709@gmail.com>
2024-08-07 22:42:53 +0530

    working on lecture upload

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -156,7 +156,9 @@ const addLectureToCourseById = async (req, res, next) => {
     if(req.file) {
         try {
             const result = await cloudinary.v2.uploader.upload(req.file.path, {
-                folder: 'lms'
+                folder: 'lms',
+                chunk_size: 50000000, // 50 mb size
+                resource_type: "video",
             });
             if (result) {
                 course.thumbnail = course.thumbnail || {};
